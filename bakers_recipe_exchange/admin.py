a503cac55dadf721ee5995db1a008ae1f91b6575
Samb <samb@townsware.com>
2022-11-03 15:23:40 -0400

    SB - Update

--- bakers_recipe_exchange/admin.py
+++ bakers_recipe_exchange/admin.py
@@ -11,8 +11,9 @@ from django_summernote.admin import SummernoteModelAdmin
 from django.contrib import admin
 from .models import Post
 
+
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'status','created_on')
+    list_display = ('title', 'slug', 'status', 'created_on')
     list_filter = ("status",)
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
