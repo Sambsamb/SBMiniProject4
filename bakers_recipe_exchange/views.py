4b738fe831f334665643b5b5c7ecf4f01b0ca844
Samb <samb@townsware.com>
2022-11-03 12:37:58 -0400

    SB - Update

--- bakers_recipe_exchange/views.py
+++ bakers_recipe_exchange/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+"""
+INF601 - Advanced Programming in Python
+Mini Project 4 - Django small web app
+Sam Boutros
+Prof. Zeller
+FHSU - Fall 2022
+11/6/2022
+"""
 
-# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the Baker's Recipe Exchange index.")
